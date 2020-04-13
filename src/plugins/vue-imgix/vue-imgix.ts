83c186f1a075419543aff328daa21db0ab0b34b0
Frederick Fogerty <frederick.fogerty@gmail.com>
2020-04-13 16:19:05 +1200

    feat: initVueImgix

--- src/plugins/vue-imgix/vue-imgix.ts
+++ src/plugins/vue-imgix/vue-imgix.ts
@@ -1,5 +1,5 @@
 import ImgixClient from 'imgix-core-js';
-import { IBuildUrlObjectResult, IImgixClientOptions, IVueImgixClient } from './types';
+import { IBuildUrlObject, IBuildUrlObjectResult, IImgixClientOptions, IVueImgixClient } from './types';
 
 class VueImgixClient implements IVueImgixClient {
   #client: ImgixClient;
@@ -7,7 +7,7 @@ class VueImgixClient implements IVueImgixClient {
     this.#client = new ImgixClient(options)
   }
 
-  buildUrlObject(url: string, options: {}): IBuildUrlObjectResult {
+  buildUrlObject(url: string, options?: {}): IBuildUrlObjectResult {
     const src = this.#client.buildURL(url, options)
     const srcset = this.#client.buildSrcSet(url, options)
 
@@ -22,6 +22,24 @@ export const buildImgixClient = (options: IImgixClientOptions) => {
 
   return client;
 };
+
+let vueImgixClientSingleton: IVueImgixClient | undefined = undefined;
+
+export const initVueImgix = (options: IImgixClientOptions) => {
+  vueImgixClientSingleton = new VueImgixClient(options)
+}
+
+const ensureVueImgixClientSingleton = (): IVueImgixClient => {
+  if (vueImgixClientSingleton == null) {
+    throw new Error('[vue-imgix] initVueImgix must be called before using exported methods. This is usually done in App.vue :)')
+  }
+  return vueImgixClientSingleton
+}
+
+export const buildUrlObject: IBuildUrlObject = (...args) => {
+  const client = ensureVueImgixClientSingleton();
+  return client.buildUrlObject(...args)
+}
 
 export type { IVueImgixClient };
 
