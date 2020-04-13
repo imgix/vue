bcec1633f89e8ba0a52605a0643003f4cc553725
Frederick Fogerty <frederick.fogerty@gmail.com>
2020-04-13 15:40:03 +1200

    chore: update dev app to include advanced usage

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,26 @@
 <template>
   <div id="app">
     Hello, world.
+
+    <h2>Advanced Usage</h2>
+    <img
+      src="advancedSrcObject.src"
+      srcset="advancedSrcObject.srcset"
+      sizes="50vw"
+    />
   </div>
 </template>
 
 <script>
+import { buildUrlObject } from '@/plugins/vue-imgix';
+
 export default {
   name: 'App',
+
+  computed: {
+    advancedSrcObject: buildUrlObject(
+      'https://assets.imgix.net/examples/pione.jpg',
+    ),
+  },
 };
 </script>
