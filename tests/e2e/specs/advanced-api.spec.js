abe78d3226966331fbe6cb8793310e79ff88a451
Frederick Fogerty <frederick.fogerty@gmail.com>
2020-04-15 10:56:40 +1200

    test(e2e): remove stub for img request

--- tests/e2e/specs/advanced-api.spec.js
+++ tests/e2e/specs/advanced-api.spec.js
@@ -3,12 +3,6 @@ describe('Advanced API', () => {
     cy.fixture('test-image').as('testImage');
   });
   it('renders an image', () => {
-    cy.server({ force404: true });
-
-    cy.route('https://assets.imgix.net/examples/test-image.jpg').as(
-      'testImage',
-    );
-
     cy.visit('/');
     cy.findByTestId('advanced-basic-image').then($image => {
       expect($image.attr('src')).to.match(/ixlib=vue/);
