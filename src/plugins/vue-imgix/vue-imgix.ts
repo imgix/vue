0f2c19f88590c5458cb1411ded4b96112097798c
Frederick Fogerty <frederick.fogerty@gmail.com>
2020-04-15 10:40:54 +1200

    feat: implement ixlib

--- src/plugins/vue-imgix/vue-imgix.ts
+++ src/plugins/vue-imgix/vue-imgix.ts
@@ -1,17 +1,29 @@
 import ImgixClient from 'imgix-core-js';
-import { IBuildUrlObject, IBuildUrlObjectResult, IImgixClientOptions, IVueImgixClient } from './types';
+import {
+  IBuildUrlObject,
+  IBuildUrlObjectResult,
+  IImgixClientOptions,
+  IVueImgixClient,
+} from './types';
+
+const VERSION = '0.1.0';
 
 class VueImgixClient implements IVueImgixClient {
-  #client: ImgixClient;
+  client: ImgixClient;
   constructor(options: IImgixClientOptions) {
-    this.#client = new ImgixClient(options)
+    this.client = new ImgixClient({
+      ...options,
+    });
+    // This is not a public API, so it is not included in the type definitions for ImgixClient
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    (this.client as any).settings.libraryParam = `vue-${VERSION}`;
   }
 
   buildUrlObject(url: string, options?: {}): IBuildUrlObjectResult {
-    const src = this.#client.buildURL(url, options)
-    const srcset = this.#client.buildSrcSet(url, options)
+    const src = this.client.buildURL(url, options);
+    const srcset = this.client.buildSrcSet(url, options);
 
-    return {src, srcset}
+    return { src, srcset };
   }
 }
 
@@ -26,20 +38,21 @@ export const buildImgixClient = (options: IImgixClientOptions) => {
 let vueImgixClientSingleton: IVueImgixClient | undefined = undefined;
 
 export const initVueImgix = (options: IImgixClientOptions) => {
-  vueImgixClientSingleton = new VueImgixClient(options)
-}
+  vueImgixClientSingleton = new VueImgixClient(options);
+};
 
 const ensureVueImgixClientSingleton = (): IVueImgixClient => {
   if (vueImgixClientSingleton == null) {
-    throw new Error('[vue-imgix] initVueImgix must be called before using exported methods. This is usually done in App.vue :)')
+    throw new Error(
+      '[vue-imgix] initVueImgix must be called before using exported methods. This is usually done in App.vue :)',
+    );
   }
-  return vueImgixClientSingleton
-}
+  return vueImgixClientSingleton;
+};
 
 export const buildUrlObject: IBuildUrlObject = (...args) => {
   const client = ensureVueImgixClientSingleton();
-  return client.buildUrlObject(...args)
-}
+  return client.buildUrlObject(...args);
+};
 
-export type { IVueImgixClient };
-
+export { IVueImgixClient };
