4e643993aae8152cdf8b94724f49dea6399f75fc
Frederick Fogerty <frederick.fogerty@gmail.com>
2020-04-17 14:03:20 +1200

    test(e2e): add test for buildURL

--- src/App.vue
+++ src/App.vue
@@ -3,17 +3,21 @@
     <h1>Advanced Usage</h1>
     <h2>buildUrlObject</h2>
     <advanced-build-url-object></advanced-build-url-object>
+    <h2>buildUrl</h2>
+    <advanced-build-url />
   </div>
 </template>
 
 <script>
 import advancedBuildUrlObject from './components/advanced/buildUrlObject';
+import advancedBuildUrl from './components/advanced/buildUrl';
 
 export default {
   name: 'App',
 
   components: {
     advancedBuildUrlObject,
+    advancedBuildUrl,
   },
 
   computed: {},
