62981aae7eb7926462fb0ac1c4d1a61ae45310dd
Frederick Fogerty <frederick.fogerty@gmail.com>
2020-04-17 13:56:31 +1200

    test(e2e): better organization

--- tests/e2e/specs/advanced-api.spec.js
+++ tests/e2e/specs/advanced-api.spec.js
@@ -2,10 +2,13 @@ describe('Advanced API', () => {
   before(() => {
     cy.fixture('test-image').as('testImage');
   });
-  it('renders an image', () => {
-    cy.visit('/');
-    cy.findByTestId('advanced-basic-image').then($image => {
-      expect($image.attr('src')).to.match(/ixlib=vue/);
+
+  context('buildUrlObject', () => {
+    it('renders an image', () => {
+      cy.visit('/');
+      cy.findByTestId('advanced-basic-image').then($image => {
+        expect($image.attr('src')).to.match(/ixlib=vue/);
+      });
     });
   });
 });
