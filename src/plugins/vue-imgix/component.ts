a942d785f32f8061d811adfa333708e386925e8c
Frederick Fogerty <frederick.fogerty@gmail.com>
2020-04-20 16:00:10 +1200

    feat: pass src and srcset to img

--- src/plugins/vue-imgix/component.ts
+++ src/plugins/vue-imgix/component.ts
@@ -1,9 +1,37 @@
 import Vue, { CreateElement } from 'vue';
 import Component from 'vue-class-component';
+import { ensureVueImgixClientSingleton, IVueImgixClient } from './vue-imgix';
+
+const ImgixProps = Vue.extend({
+  props: {
+    src: {
+      type: String,
+      required: true,
+    },
+    imgixParams: Object,
+  },
+});
 
 @Component
-export class Imgix extends Vue {
+export class Imgix extends ImgixProps {
+  // Using !: here because we ensure it is set in created()
+  private vueImgixSingleton!: IVueImgixClient;
+
+  created() {
+    this.vueImgixSingleton = ensureVueImgixClientSingleton();
+  }
+
   render(createElement: CreateElement) {
-    return createElement('img');
+    const { src, srcset } = this.vueImgixSingleton.buildUrlObject(
+      this.src,
+      this.imgixParams,
+    );
+
+    return createElement('img', {
+      attrs: {
+        src,
+        srcset,
+      },
+    });
   }
 }
