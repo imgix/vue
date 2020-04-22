7f7e285a1a4e187c495fcd2f0eda4ee9534794b3
Frederick Fogerty <frederick.fogerty@gmail.com>
2020-04-23 10:36:32 +1200

    config(rollup): add rollup config

---
+++ build/rollup.config.js
@@ -0,0 +1,23 @@
+import commonjs from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+import typescript from '@rollup/plugin-typescript';
+import babel from 'rollup-plugin-babel';
+import vue from 'rollup-plugin-vue';
+export default {
+  input: 'src/plugins/vue-imgix/wrapper.ts',
+  output: {
+    name: 'Imgix',
+    exports: 'named',
+  },
+  plugins: [
+    resolve(),
+    typescript(),
+    commonjs(),
+    vue({
+      compileTemplate: true, // Explicitly convert template to render function
+    }),
+    babel({
+      runtimeHelpers: true,
+    }),
+  ],
+};
