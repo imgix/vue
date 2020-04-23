f6b6fa4ed6a18dd6d9999a729f8498381efce228
Frederick Fogerty <frederick.fogerty@gmail.com>
2020-04-24 11:21:37 +1200

    test(e2e): fixed width

--- tests/e2e/specs/simple-api.spec.js
+++ tests/e2e/specs/simple-api.spec.js
@@ -6,4 +6,11 @@ describe('Simple API', () => {
       expect($image.attr('srcset')).to.match(/ixlib=vue/);
     });
   });
+
+  it('renders a fixed width image', () => {
+    cy.findByTestId('simple-fixed-width').then($image => {
+      expect($image.attr('src')).to.match(/w=100/);
+      expect($image.attr('width')).to.eq('100');
+    });
+  });
 });
