112615b33f81ef8b2c8f1336071504389cc7300d
Frederick Fogerty <frederick.fogerty@gmail.com>
2020-04-24 11:10:21 +1200

    test: fixed mode

--- tests/unit/imgix-component.spec.ts
+++ tests/unit/imgix-component.spec.ts
@@ -56,8 +56,8 @@ describe('imgix component', () => {
   it('imgixParams should be set on the rendered src and srcset', () => {
     const wrapper = render(Imgix, {
       propsData: {
-        src: 'examples/pione.jpg',
         'data-testid': 'img-rendering',
+        src: 'examples/pione.jpg',
         imgixParams: { crop: 'faces' },
       },
     });
@@ -70,5 +70,63 @@ describe('imgix component', () => {
       'srcset',
       expect.stringMatching('crop=faces'),
     );
+  });
+
+  describe('in fixed mode (width or height passed to url, imgixParams, or dom attribute)', () => {
+    it('when a width passed to imgixParams', () => {
+      const wrapper = render(Imgix, {
+        propsData: {
+          'data-testid': 'img-rendering',
+          src: 'examples/pione.jpg',
+          imgixParams: {
+            w: 100,
+          },
+        },
+      });
+
+      expect(wrapper.getByTestId('img-rendering')).toHaveAttribute(
+        'src',
+        expect.stringMatching('w=100'),
+      );
+      const srcset = wrapper
+        .getByTestId('img-rendering')
+        .getAttribute('srcset');
+
+      expect(srcset).not.toBeFalsy();
+      if (!srcset) {
+        fail('srcset is null');
+      }
+      const firstSrcSet = srcset.split(',').map(v => v.trim())[0];
+      expect(firstSrcSet).toMatch('w=100');
+      expect(firstSrcSet).toMatch('dpr=1');
+      expect(firstSrcSet).toMatch(' 1x');
+    });
+
+    it('when a width passed to element', () => {
+      const wrapper = render(Imgix, {
+        propsData: {
+          'data-testid': 'img-rendering',
+          src: 'examples/pione.jpg',
+          width: 100,
+        },
+      });
+
+      expect(wrapper.getByTestId('img-rendering')).toHaveAttribute(
+        'src',
+        expect.stringMatching('w=100'),
+      );
+      const srcset = wrapper
+        .getByTestId('img-rendering')
+        .getAttribute('srcset');
+
+      expect(srcset).not.toBeFalsy();
+      if (!srcset) {
+        fail('srcset is null');
+      }
+      const firstSrcSet = srcset.split(',').map(v => v.trim())[0];
+      expect(firstSrcSet).toMatch('w=100');
+      expect(firstSrcSet).toMatch('dpr=1');
+      expect(firstSrcSet).toMatch(' 1x');
+    });
   });
 });
