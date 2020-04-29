a0efec3603172d1482579d1dfd73646c680516d2
Frederick Fogerty <frederick.fogerty@gmail.com>
2020-04-30 11:14:38 +1200

    refactor: remove window initalization for now

--- src/plugins/vue-imgix/index.ts
+++ src/plugins/vue-imgix/index.ts
@@ -18,19 +18,6 @@ const plugin = {
   install,
 };
 
-// Auto-install when vue is found (eg. in browser via <script> tag)
-// let GlobalVue = null;
-// if (typeof window !== 'undefined') {
-//   GlobalVue = window.Vue;
-//   // @ts-ignore
-// } else if (typeof global !== 'undefined') {
-//   // @ts-ignore
-//   GlobalVue = global.Vue;
-// }
-// if (GlobalVue) {
-//   GlobalVue.use(plugin);
-// }
-
 // To allow use as module (npm/webpack/etc.) export component
 export default plugin;
 export * from './vue-imgix';
