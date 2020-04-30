7acf29b8858babfff7176d463470e75231383cf9
Frederick Fogerty <frederick.fogerty@gmail.com>
2020-05-01 10:35:19 +1200

    chore: fix rollup build

--- build/rollup.config.js
+++ build/rollup.config.js
@@ -4,7 +4,7 @@ import typescript from '@rollup/plugin-typescript';
 import babel from 'rollup-plugin-babel';
 import vue from 'rollup-plugin-vue';
 export default {
-  input: 'src/plugins/vue-imgix',
+  input: 'src/plugins/vue-imgix/index.ts',
   output: {
     name: 'Imgix',
     exports: 'named',
