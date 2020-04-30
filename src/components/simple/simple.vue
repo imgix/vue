4740372c2be1a8d31239fc635c9813370cf2a2b0
Frederick Fogerty <frederick.fogerty@gmail.com>
2020-05-01 11:03:39 +1200

    chore: lint

--- src/components/simple/simple.vue
+++ src/components/simple/simple.vue
@@ -3,7 +3,11 @@
     <h2>Simple</h2>
     <ix-img src="examples/pione.jpg" sizes="20vw" data-testid="simple-fluid" />
     <h2>Fixed</h2>
-    <ix-img src="examples/pione.jpg" width="100" data-testid="simple-fixed-width" />
+    <ix-img
+      src="examples/pione.jpg"
+      width="100"
+      data-testid="simple-fixed-width"
+    />
   </div>
 </template>
 
