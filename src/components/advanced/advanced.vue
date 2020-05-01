dcd1dc0f25c0fa6f1fe8221147f30a9171bb2cc4
Frederick Fogerty <frederick.fogerty@gmail.com>
2020-05-01 15:45:21 +1200

    test(e2e): custom attribute mapping

---
+++ src/components/advanced/advanced.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h2>attributeConfig</h2>
+    <div data-testid="advanced-attribute-config">
+      <ix-img
+        src="examples/pione.jpg"
+        :attributeConfig="{
+          src: 'data-src',
+          srcset: 'data-srcset',
+        }"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'advance-api',
+};
+</script>
