833ad12bd00118fa9cd3bee3373acc5db3eb1ada
Frederick Fogerty <frederick.fogerty@gmail.com>
2020-05-01 14:04:47 +1200

    test(unit): min/max srcset widths for buildSrcSet

--- tests/unit/build-src-set.spec.ts
+++ tests/unit/build-src-set.spec.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 import {
   buildImgixClient,
   IVueImgixClient,
@@ -18,5 +19,35 @@ describe('buildSrcSet', () => {
     expect(firstSrcSet[0]).toMatch(/assets.imgix.net\/examples\/pione.jpg/);
     expect(firstSrcSet[0]).toMatch(/w=[0-9]/);
     expect(firstSrcSet[1]).toMatch(/^[0-9]+w$/);
+  });
+
+  it('custom widths are passed to imgix-core-js', () => {
+    jest.resetModules();
+    jest.mock('imgix-core-js');
+    const { buildImgixClient } = require('@/plugins/vue-imgix/');
+    const ImgixClient = require('imgix-core-js');
+    const ImgixMock = {
+      settings: {},
+      buildSrcSet: jest.fn(),
+      buildURL: jest.fn(),
+    };
+    ImgixClient.mockImplementation(() => ImgixMock);
+
+    const client = buildImgixClient({
+      domain: 'testing.imgix.net',
+    });
+
+    client.buildSrcSet('image.jpg', {}, { widths: [100, 200] });
+
+    expect(ImgixMock.buildSrcSet).toHaveBeenCalledWith(
+      expect.anything(),
+      expect.anything(),
+      expect.objectContaining({
+        widths: [100, 200],
+      }),
+    );
+
+    jest.resetAllMocks();
+    jest.resetModules();
   });
 });
