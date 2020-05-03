ed82c374334c32f5cb6a4b0288b2df9eedb40306
Frederick Fogerty <frederick.fogerty@gmail.com>
2020-05-04 09:49:00 +1200

    test(e2e): picture mode

--- tests/e2e/specs/simple-api.spec.js
+++ tests/e2e/specs/simple-api.spec.js
@@ -13,4 +13,11 @@ describe('Simple API', () => {
       expect($image.attr('width')).to.eq('100');
     });
   });
+
+  it('renders a picture', () => {
+    const picture = cy.findByTestId('simple-picture').find('picture');
+
+    picture.find('source:first').should('have.attr', 'srcset', /ixlib/);
+    picture.find('img').should('have.attr', 'src', /ixlib/);
+  });
 });
