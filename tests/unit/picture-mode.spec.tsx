b67eade56f16f460a917a7746ad1814254f3f818
Frederick Fogerty <frederick.fogerty@gmail.com>
2020-05-06 10:04:41 +1200

    test(unit): first picture test

---
+++ tests/unit/picture-mode.spec.tsx
@@ -0,0 +1,23 @@
+import VueImgix from '@/plugins/vue-imgix';
+import '@testing-library/jest-dom';
+import { render } from '@testing-library/vue';
+import Vue from 'vue';
+describe('imgix component', () => {
+  beforeAll(() => {
+    Vue.use(VueImgix, {
+      domain: 'assets.imgix.net',
+    });
+  });
+
+  it('should render a picture', () => {
+    const wrapper = render(
+      Vue.component('test-component', {
+        render() {
+          return <ix-picture data-testid="test-picture" />;
+        },
+      }),
+    );
+
+    expect(wrapper.getByTestId('test-picture').tagName).toBe('PICTURE');
+  });
+});
