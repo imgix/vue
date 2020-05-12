12a26614ce2af9703d702f564808a23fdd8496d5
Frederick Fogerty <frederick.fogerty@gmail.com>
2020-05-13 11:14:45 +1200

    chore: update lint settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,8 +14,8 @@ module.exports = {
     ecmaVersion: 2020,
   },
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-console': 'error',
+    'no-debugger': 'error',
     '@typescript-eslint/interface-name-prefix': 'off',
   },
   overrides: [
