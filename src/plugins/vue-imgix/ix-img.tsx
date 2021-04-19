1710de759ce8e3a57dc628c1e301a10e5ed936e4
Luis Ball <lball@imgix.com>
2021-04-19 13:59:54 -0400

    chore: attempt to remove vue-class-component
    The vue-class-component docs are confusing at this time. This was
    an attempt to remove this dependency. It was not successful.

--- src/plugins/vue-imgix/ix-img.tsx
+++ src/plugins/vue-imgix/ix-img.tsx
@@ -1,37 +1,27 @@
+import { defineComponent, h } from 'vue';
 import { ensureVueImgixClientSingleton, IVueImgixClient } from './vue-imgix';
-import { Vue, prop } from 'vue-class-component';
 
-const Props = {
-  src: prop({ type: String, required: true }),
-  fixed:  prop({type: Boolean}),
-  imgixParams:  prop({type: Object}),
-  width:  prop({ [String, Number]}),
-  height:  prop({ [String, Number]}),
-  attributeConfig:  prop({type: Object}),
-  disableVariableQuality:  prop({type: Boolean}),
-};
-
-// const IxImgProps = defineComponent({
-//   props: {
-//     src: {
-//       type: String,
-//       required: true,
-//     },
-//     fixed: Boolean,
-//     imgixParams: Object,
-//     width: [String, Number],
-//     height: [String, Number],
-//     attributeConfig: Object,
-//     disableVariableQuality: Boolean,
-//   },
-// });
+const IxImgProps = defineComponent({
+  props: {
+    src: {
+      type: String,
+      required: true,
+    },
+    fixed: Boolean,
+    imgixParams: Object,
+    width: [String, Number],
+    height: [String, Number],
+    attributeConfig: Object,
+    disableVariableQuality: Boolean,
+  },
+});
 
 const defaultAttributeMap = {
   src: 'src',
   srcset: 'srcset',
 };
 
-export class IxImg extends Vue.with(Props) {
+export class IxImg extends IxImgProps {
   // Using !: here because we ensure it is set in created()
   private vueImgixSingleton!: IVueImgixClient;
 
