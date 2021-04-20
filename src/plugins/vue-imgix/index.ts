b8370ea8d18ea5b7f9a85131eebb7e82f9125ab4
Luis Ball <lball@imgix.com>
2021-04-20 14:27:54 -0400

    refactor: use app instead of vue in index file
    This commit removes references to the Global `Vue` argument and replaces
    it with references to the `App` argument. This is, again, becaues the
    Vue 3 Global API deprecated access to `Vue`. The components are
    registered to the app using the same `.component` API as before.

--- src/plugins/vue-imgix/index.ts
+++ src/plugins/vue-imgix/index.ts
@@ -1,17 +1,17 @@
-import _Vue from 'vue';
+import _App from './App.vue';
 import { IxPicture } from './ix-picture';
 import { IxSource } from './ix-source';
 import { IVueUseImgixOptions } from './types';
 import { initVueImgix, IxImg } from './vue-imgix';
 
 // Declare install function executed by Vue.use()
-export function install(Vue: typeof _Vue, options: IVueUseImgixOptions) {
+export function install(App: typeof _App, options: IVueUseImgixOptions) {
   if (install.installed) return;
   install.installed = true;
   initVueImgix(options);
-  Vue.component('ix-img', IxImg);
-  Vue.component('ix-picture', IxPicture);
-  Vue.component('ix-source', IxSource);
+  App.component('ix-img', IxImg);
+  App.component('ix-picture', IxPicture);
+  App.component('ix-source', IxSource);
 }
 install.installed = false;
 
