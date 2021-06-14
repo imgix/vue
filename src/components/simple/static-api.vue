a896c09c7e88ed8a868ef4535d971fc6b6bfaac6
Luis Ball <lball@imgix.com>
2021-06-14 14:54:36 -0700

    chore: add example partial path api component

---
+++ src/components/simple/static-api.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h2>2-step URL API Example</h2>
+    <ix-img
+      src="/examples/pione.jpg"
+      width="100"
+      data-testid="2-step-api"
+    />
+    <ix-img
+      src="https://assets.imgix.net/examples/pione.jpg"
+      width="100"
+      data-testid="2-step-api"
+    />
+    <ix-img
+      src="https://sdk-test.imgix.net/amsterdam.jpg"
+      width="100"
+      data-testid="2-step-api"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: '2-step-api',
+};
+</script>
