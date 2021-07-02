7b4dd29630135b161c8fd7fe5ca6217312ba82c9
Luis Ball <lball@imgix.com>
2021-07-02 11:43:39 -0700

    feat: use createApp api
    This commit uses vue 3 createApp api instead of vue 2 Vue.use. The
    productionTip config has also been removed from vue 3.

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,14 @@
 import VueImgix from '@/plugins/vue-imgix';
-import Vue from 'vue';
+import { createApp } from 'vue';
 import App from './App.vue';
 
-Vue.config.productionTip = false;
+export const app = createApp(App);
 
-Vue.use(VueImgix, {
-  domain: 'assets.imgix.net',
-  defaultIxParams: {
-    auto: 'format',
-  },
-});
-
-new Vue({
-  render: (h) => h(App),
-}).$mount('#app');
+app
+  .use(VueImgix, {
+    domain: 'assets.imgix.net',
+    defaultIxParams: {
+      auto: 'format',
+    },
+  })
+  .mount('#app');
