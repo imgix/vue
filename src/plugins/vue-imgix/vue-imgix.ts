3b4e4a870d5e0137ec1d7351f55a485192be1bcf
imgix-git-robot <support@imgix.com>
2021-07-08 21:42:37 +0000

    chore(release): 2.9.0
    ## [2.9.0](https://github.com/imgix/vue-imgix/compare/v2.8.4...v2.9.0) (2021-07-08)
    
    ### Features
    
    * create static build URL and srcset methods ([3d22bb1](https://github.com/imgix/vue-imgix/commit/3d22bb147327c5ddf10f35c86626b8e2d069a431))

--- src/plugins/vue-imgix/vue-imgix.ts
+++ src/plugins/vue-imgix/vue-imgix.ts
@@ -13,7 +13,7 @@ import {
 import type { IVueImgixClient } from './types';
 
 // Do not change this
-const VERSION = '2.8.4';
+const VERSION = '2.9.0';
 
 const clientOptionDefaults = {
   includeLibraryParam: true,
