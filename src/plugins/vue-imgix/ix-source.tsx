0b7cb7e9b248365b2276d70396426270d8873daf
Luis H. Ball Jr <luqven@gmail.com>
2021-07-08 09:59:40 -0700

    style: remove extra space
    Co-authored-by: Sherwin Heydarbeygi <sherwin@imgix.com>

--- src/plugins/vue-imgix/ix-source.tsx
+++ src/plugins/vue-imgix/ix-source.tsx
@@ -35,6 +35,6 @@ export const IxSource = defineComponent({
       [attributeConfig.srcset]: srcset,
     };
 
-    return () => h('source',  childAttrs );
+    return () => h('source', childAttrs );
   },
 });
