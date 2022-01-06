16a2e4aad8c549cafd5ee2b46075bf7fce25fb89
Luis Ball <lball@imgix.com>
2022-01-05 23:01:45 -0500

    test: create ix-picture component for spec

---
+++ src/components/simple/ix-picture.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h2>Picture</h2>
+    <div data-testid="simple-picture">
+      <ix-picture>
+        <ix-source src="blog/unsplash-kiss.jpg" />
+        <ix-img src="blog/unsplash-kiss.jpg" />
+      </ix-picture>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'ix-picture-simple',
+  };
+</script>
