837246a0e7e341e4ede49db1a513188c88e29bb6
Luis H. Ball Jr <lball@imgix.com>
2022-03-10 10:54:52 -0500

    chore: change comments to be html comments
    Co-authored-by: Sherwin Heydarbeygi <sherwin@imgix.com>

--- src/components/simple/static-api.vue
+++ src/components/simple/static-api.vue
@@ -1,13 +1,13 @@
 <template>
   <div>
     <h2>Absolute URL Static API Example</h2>
-    // partial URL gets domain from client
+    <!–– partial URL gets domain from client -->
     <ix-img
       src="/examples/pione.jpg"
       width="100"
       data-testid="static-api-relative"
     />
-    // absolute URL domain does not get overridden
+    <!-- absolute URL domain does not get overridden -->
     <ix-img
       src="https://sdk-test.imgix.net/amsterdam.jpg"
       width="100"
