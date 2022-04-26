c65372045e991a134e7f54235267717feffb769b
imgix-git-robot <support@imgix.com>
2022-04-26 07:37:33 +0000

    chore(release): 3.0.0-rc.5
    ## [3.0.0-rc.5](https://github.com/imgix/vue-imgix/compare/v3.0.0-rc.4...v3.0.0-rc.5) (2022-04-26)

--- dist/vue-imgix.umd.js
+++ dist/vue-imgix.umd.js
@@ -1678,7 +1678,7 @@
 
   function objectWithoutProperties (obj, exclude) { var target = {}; for (var k in obj) if (Object.prototype.hasOwnProperty.call(obj, k) && exclude.indexOf(k) === -1) target[k] = obj[k]; return target; }
   // Do not change this
-  var VERSION = '3.0.0-rc.3';
+  var VERSION = '3.0.0-rc.4';
   var clientOptionDefaults = {
       includeLibraryParam: true,
   };
--- src/plugins/vue-imgix/vue-imgix.ts
+++ src/plugins/vue-imgix/vue-imgix.ts
@@ -13,7 +13,7 @@ import {
 } from './types';
 
 // Do not change this
-const VERSION = '3.0.0-rc.4';
+const VERSION = '3.0.0-rc.5';
 
 const clientOptionDefaults = {
   includeLibraryParam: true,
